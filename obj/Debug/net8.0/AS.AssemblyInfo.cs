f11176cd46776063c2e53e49ac5b147b98fa0790
Mateus Bueno <mateusbuenog@rede.ulbra.br>
2024-07-10 20:33:38 -0300

    First Commit

--- obj/Debug/net8.0/AS.AssemblyInfo.cs
+++ obj/Debug/net8.0/AS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+072ac0d6f27579ea8f61f246890ca20370047df2")]
 [assembly: System.Reflection.AssemblyProductAttribute("AS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
